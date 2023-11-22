c38989cb6a722e50860e1130c2280ff8686f70d7
vitor <eletrotecvitor@gmail.com>
2023-11-22 19:19:37 -0300

    finalizando o teste de edição

--- cypress/e2e/Agenda/Edicao.cy.js
+++ cypress/e2e/Agenda/Edicao.cy.js
@@ -5,7 +5,14 @@ describe('Testes para função Alterar contato cadastrado', () => {
         cy.visit('https://agenda-contatos-react.vercel.app/')
     })
     
-    it('Liberar dados do contato para ser alterado', () => {
-        cy.get('.edit').click()
+    it('Alterar dados de um Contato', () => {
+        cy.get('.edit').click({ multiple: true })
+        cy.get('[type="text"]').clear()
+        cy.get('[type="email"]').clear()
+        cy.get('[type="tel"]').clear()
+        cy.get('input[placeholder="Nome"]').type('Vitor Aguiar')
+        cy.get('input[placeholder="E-mail"]').type('vitor@gmail.com')
+        cy.get('input[placeholder="Telefone"]').type('(21)99999-9999')
+        cy.get('.alterar').click()
     })
 })
